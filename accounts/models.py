9ba25e7d687331a230187964488cddc0e4a06646
DarkestTiger <june140133@gmail.com>
2024-05-14 18:22:04 +0900

    accounts:User -update 회원가입,로그인,검증,로그아웃 구현 완료

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+LOCAL_HOST="http://127.0.0.1:8000/"
 
 # 유저모델
 class User(AbstractUser):
     username = models.CharField(unique=True, max_length=30)
     email = models.EmailField(unique=True)
-    profile_img = models.ImageField(upload_to="/midea/profile")
-    introduce = models. models.TextField(max_lenth=300)
+    profile_img = models.ImageField(upload_to="media\profile")
+    introduce = models.TextField(blank=True)
     follower = models.ManyToManyField("self", symmetrical=False,blank=True,related_name='followers')
 
     def __str__(self):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from accounts import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path("/signup/", views.UserSignUp.as_view(), name='signup')
+    path("auth/signup/", views.UserSignUp.as_view(), name='signup'),
+    path("auth/login/", views.UserLogIn.as_view(), name='logout'),
+    path("auth/logout/", views.UserLogOut.as_view(), name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
