cd79cdc435b2487677a842497174d67cee35e633
gozzun <gozzun_1256@naver.com>
2024-05-23 15:45:11 +0900

    accounts:프로필 이미지 수정 완료

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.templatetags.static import static
 
 LOCAL_HOST="http://127.0.0.1:8000/"
 
@@ -7,10 +8,16 @@ LOCAL_HOST="http://127.0.0.1:8000/"
 class User(AbstractUser):
     username = models.CharField(unique=True, max_length=30)
     email = models.EmailField(unique=True)
-    profile_img = models.ImageField(upload_to="profile/images/")
+    profile_img = models.ImageField(upload_to="profile/images/", blank=True)
 
     address = models.CharField(max_length=300)
     introduce = models.TextField(blank=True)
+
+    def get_profile_url(self):
+        if self.profile_img is None:
+            self.profile_img.url = static('user.png')
+        else:
+            return self.profile_img.url
 
 # related_name을 following로 수정
     follower = models.ManyToManyField("self", symmetrical=False,blank=True,related_name='following')
